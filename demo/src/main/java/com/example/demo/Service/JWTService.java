e4515933470b390b7b89cada0ac5358495c742a0
oshanLahiru0307 <klahiruoshan@gmail.com>
2026-08-01 00:16:50 +0530

    jwt service functions are ready

---
+++ demo/src/main/java/com/example/demo/Service/JWTService.java
@@ -0,0 +1,44 @@
+package com.example.demo.Service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Map;
+
+@Service
+public class JWTService {
+
+    private static final String secretKey = "adxahhwhjwjdhs";
+
+    public SecretKey getSecretKey(){
+        return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String GenerateToken(Map<String, Object> claim, String email){
+        return Jwts.builder()
+                .claims(claim)
+                .subject(email)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+360000))
+                .signWith(getSecretKey())
+                .compact();
+    }
+
+    public String ExtractUserEmail(String token){
+        return Jwts.parser()
+                .verifyWith(getSecretKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token, String email){
+        String userEmail = ExtractUserEmail(token);
+        return userEmail.equals(email);
+    }
+}
