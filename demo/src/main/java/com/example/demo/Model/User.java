f59194fbc18c7aa21366b43a5c20e6179abbdac5
oshanLahiru0307 <klahiruoshan@gmail.com>
2026-07-31 10:27:42 +0530

    intial commit message

---
+++ demo/src/main/java/com/example/demo/Model/User.java
@@ -0,0 +1,24 @@
+package com.example.demo.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    private String password;
+    private String email;
+}
---
+++ demo/src/main/java/com/example/demo/Repository/UserRepo.java
@@ -0,0 +1,14 @@
+package com.example.demo.Repository;
+
+import com.example.demo.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String Email);
+    Optional<User> findByUsername(String Username);
+}
