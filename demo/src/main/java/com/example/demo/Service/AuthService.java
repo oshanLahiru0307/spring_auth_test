bd0fe4e4f05515e5ecadceb2ff98e21a3e18d996
oshanLahiru0307 <klahiruoshan@gmail.com>
2026-08-02 19:56:09 +0530

    auth service functions are updated

--- demo/src/main/java/com/example/demo/Service/AuthService.java
+++ demo/src/main/java/com/example/demo/Service/AuthService.java
@@ -1,11 +1,14 @@
 package com.example.demo.Service;
 
 import com.example.demo.DTO.AuthResponse;
+import com.example.demo.DTO.LoginRequest;
 import com.example.demo.DTO.RegisterRequest;
 import com.example.demo.Model.UserModel;
 import com.example.demo.Repository.UserRepo;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +26,10 @@ public class AuthService {
     private ModelMapper modelMapper;
     @Autowired
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
     //register service function
     public AuthResponse RegisterUser(RegisterRequest registerRequest){
         UserModel userData = modelMapper.map(registerRequest, UserModel.class);
@@ -31,10 +38,24 @@ public class AuthService {
         }
         userData.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
         userRepo.save(userData);
-        String email = userData.getEmail();
-        Map<String, Object> claims = new HashMap<>();
-        claims.put("email", email);
-        String token = jwtService.GenerateToken(claims, email);
+        Map<String, Object> regClaims = new HashMap<>();
+        regClaims.put("name", userData.getName());
+        String token = jwtService.GenerateToken(regClaims, userData.getEmail());
+        return new AuthResponse(token);
+    }
+
+    //login service function
+    public AuthResponse LoginUser(LoginRequest loginRequest){
+        UserModel userData = userRepo.findByEmail(loginRequest.getEmail()).orElseThrow(
+                ()-> new RuntimeException("user not Found with Email: " + loginRequest.getEmail())
+        );
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword())
+        );
+        Map<String, Object> logClaims = new HashMap<>();
+        logClaims.put("name", userData.getName());
+        logClaims.put("id", userData.getId());
+        String token = jwtService.GenerateToken(logClaims, userData.getEmail());
         return new AuthResponse(token);
     }
 }
